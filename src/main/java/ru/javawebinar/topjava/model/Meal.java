836edec257112cb655c1ef8dd76f787bfdff7110
Andrey <glanbor@mail.ru>
2022-02-18 17:43:41 +0300

    HW 03 initDB and realise JdbcMealRepository

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,11 +5,11 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity {
-    private final LocalDateTime dateTime;
+    private LocalDateTime dateTime;
 
-    private final String description;
+    private String description;
 
-    private final int calories;
+    private int calories;
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
@@ -22,6 +22,9 @@ public class Meal extends AbstractBaseEntity {
         this.calories = calories;
     }
 
+    public Meal() {
+    }
+
     public LocalDateTime getDateTime() {
         return dateTime;
     }
@@ -32,6 +35,18 @@ public class Meal extends AbstractBaseEntity {
 
     public int getCalories() {
         return calories;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
     }
 
     public LocalDate getDate() {
