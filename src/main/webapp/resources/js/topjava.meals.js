3043d110e983bde1b8ac4956c9b0aa620678e0fe
Andrey <glanbor@mail.ru>
2022-04-02 11:28:30 +0300

    Realise HW08_1

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,49 @@
+const mealAjaxUrl = "profile/meals/";
+
+// https://stackoverflow.com/a/5064235/548473
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+function applyFilters() {
+    $.ajax({
+        type: "GET",
+        url: ctx.ajaxUrl + "filter",
+        data: $("#filter").serialize()
+    }).done(updateTable);
+}
+
+// $(document).ready(function () {
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ]
+        })
+    );
+});
